3ef568e5de355d8a62f0ddbb1bb5b745105dd1de
Agrey Tosira <agreytosira@gmail.com>
2023-11-24 08:42:35 +0700

    feat: add edit notes by id handler

--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,39 @@
 const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const { title, tags, body } = request.payload;
+    const updatedAt = new Date().toISOString();
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil diperbarui'
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui catatan. Id tidak ditemukan'
+    });
+    response.code(404);
+    return response;
+};
+
 const getAllNotesHandler = () => ({
     status: 'success',
     data: {
@@ -71,4 +104,9 @@ const addNoteHandler = (request, h) => {
     return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
+module.exports = {
+    addNoteHandler,
+    getAllNotesHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler
+};
