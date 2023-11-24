2f4ec3ff60a48443dfd9e63af86af0785929d5c3
Agrey Tosira <agreytosira@gmail.com>
2023-11-24 08:37:25 +0700

    feat: add getNoteById handler

--- src/handler.js
+++ src/handler.js
@@ -9,6 +9,28 @@ const getAllNotesHandler = () => ({
     }
 });
 
+const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const note = notes.filter((n) => n.id === id)[0];
+
+    if (note !== undefined) {
+        return {
+            status: 'success',
+            data: {
+                note
+            }
+        };
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan tidak ditemukan'
+    });
+    response.code(404);
+    return response;
+};
+
 const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
 
@@ -49,4 +71,4 @@ const addNoteHandler = (request, h) => {
     return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler };
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
