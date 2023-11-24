e404215ed805d0a0689438697988eba71622c7e7
Agrey Tosira <agreytosira@gmail.com>
2023-11-24 08:45:45 +0700

    feat: add delete note handler

--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,29 @@
 const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+        notes.splice(index, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil dihapus'
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan gagal dihapus. Id tidak ditemukan'
+    });
+    response.code(404);
+    return response;
+};
+
 const editNoteByIdHandler = (request, h) => {
     const { id } = request.params;
 
@@ -108,5 +131,6 @@ module.exports = {
     addNoteHandler,
     getAllNotesHandler,
     getNoteByIdHandler,
-    editNoteByIdHandler
+    editNoteByIdHandler,
+    deleteNoteByIdHandler
 };
