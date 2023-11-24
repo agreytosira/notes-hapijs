1931bb8e2ae1e6e76641396ac9f3d6bcbbb6a0f4
Agrey Tosira <agreytosira@gmail.com>
2023-11-24 08:28:26 +0700

    feat: render all notes

--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,13 @@
 const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
+const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+        notes
+    }
+});
+
 const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
 
@@ -42,4 +49,4 @@ const addNoteHandler = (request, h) => {
     return response;
 };
 
-module.exports = { addNoteHandler };
+module.exports = { addNoteHandler, getAllNotesHandler };
